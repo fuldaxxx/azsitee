f914d18173c417d1be9c60ddc1b15adbf354b8e4
Ruslan Gorbunov <fuldax17@gmail.com>
2021-09-03 23:24:08 +0300

    first commit

--- az/src/profiles/models.py
+++ az/src/profiles/models.py
@@ -3,8 +3,26 @@ from django.db import models
 
 class UserNet(AbstractUser):
     '''Custom model user'''
+
+    GENDER = (
+        ('male', 'male'),
+        ('female', 'female'),
+
+    )
     midle_name = models.CharField(max_length=50)
     first_login = models.DateTimeField(null=True)
     phone = models.CharField(max_length=14)
     avatar = models.ImageField(upload_to='user/avatar', blank=True, null=True)
-    
+    bio = models.TextField(blank=True, null=True)
+    birthday = models.DateField(blank=True, null=True)
+    gender = models.CharField(max_length=6, default='male')
+    user_permissions = []
+    groups = []
+    technology = models.ManyToManyField('Technology', related_name='users')
+
+
+class Technology(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
--- az/src/profiles/views.py
+++ az/src/profiles/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from rest_framework import permissions
+from .models import UserNet
+from .serializers import GetUserNetSerializer, GetUserNetPublicSerializer
 
-# Create your views here.
+
+class UserNetPublicView(ModelViewSet):
+    queryset = UserNet.objects.all()
+    serializer_class = GetUserNetPublicSerializer
+    permission_classes = [permissions.AllowAny]
+
+
+class UpdateUserNetView(ModelViewSet):
+    ''' Редактирвание пользовтаелья'''
+
+    serializer_class = GetUserNetSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return UserNet.objects.filter(id=self.request.user.id)
+
+
